7432e5e36825f931428fbdbd648ae0bc11388319
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-05 15:44:26 +0630

    Add AppLinks class with social media constants

---
+++ lib/core/constants/app_links.dart
@@ -0,0 +1,10 @@
+class AppLinks {
+  static const github = 'https://github.com/Dev-moe-kyawaung';
+  static const gravatar = 'https://gravatar.com/moekyawaung2026';
+  static const youtube = 'https://www.youtube.com/channel/UCuTXUguZb4xjeL2nX8WJG';
+  static const linkedin = 'https://www.linkedin.com/in/moe-kyaw-aung-2653093a1';
+  static const tumblr = 'https://www.tumblr.com/moekyawaung';
+  static const vimeo = 'https://vimeo.com/user252414232';
+  static const bluesky = 'https://bsky.app/profile/moekyawaung96.bsky.social';
+  static const paypal = 'https://www.paypal.com/paypalme/my/profile';
+}
