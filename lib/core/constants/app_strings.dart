a0678a516bf62499a5a6a2ce2b8db1d0174109a5
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-05 15:44:59 +0630

    Create app_strings.dart

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,5 @@
+class AppStrings {
+  static const name = 'မိုးကျော်အောင် · Moe Kyaw Aung';
+  static const role = 'Senior Android Developer';
+  static const location = 'Tachileik, Myanmar 🇲🇲 ↔ Bangkok, Thailand 🇹🇭';
+}
