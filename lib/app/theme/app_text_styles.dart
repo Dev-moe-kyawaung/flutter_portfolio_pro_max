b94b461f040cd67dae71740efc312f487e70ed13
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-05 15:33:38 +0630

    Add AppTextStyles class with heroTitle style

---
+++ lib/app/theme/app_text_styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const heroTitle = TextStyle(
+    fontSize: 54,
+    fontWeight: FontWeight.w900,
+    height: 1.02,
+    letterSpacing: -1.0,
+  );
+}
