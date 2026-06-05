ce4997469f40e567ac9688cfc05b19f1dcfe4e4b
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-05 15:46:23 +0630

    Add AppColors class with color constants

---
+++ lib/app/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const bg = Color(0xFF050816);
+  static const surface = Color(0xFF0B1020);
+  static const cyan = Color(0xFF00F5FF);
+  static const pink = Color(0xFFFF2BD6);
+  static const yellow = Color(0xFFFFE600);
+  static const text = Color(0xFFEAF2FF);
+}
